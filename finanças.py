2b78d13ea28ce385fc78229675abc3fb097cedb0
GABRIEL CARLOS PEREIRA <gabriel.pereira25@univille.br>
2026-06-24 10:29:40 -0300

    Terminado

--- finanças.py
+++ finanças.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import json
 from datetime import datetime
 
@@ -109,235 +108,42 @@ def adicionar_transacao(tipo):
 
     print(f"\n{tipo.capitalize()} de R$ {valor:.2f} Registrada Com Sucesso\n")
 
-
-menu()
-
- 
- 
- 
-=======
-from datetime import datetime
-
-
-
-# categorias
-# salario, luz, agua, etc...
-
-
-def cadastrar_categoria():
-    categoria = input("Digite o nome da categoria: ")
-
-    with open("categorias.txt", "a", encoding="utf-8") as arquivo:
-        arquivo.write(categoria + "\n")
-
-    print("Categoria cadastrada com sucesso!")
-
-
-def listar_categorias():
-    try:
-        with open("categorias.txt", "r", encoding="utf-8") as arquivo:
-            categorias = arquivo.readlines()
-
-        if len(categorias) == 0:
-            print("Nenhuma categoria cadastrada.")
-            return []
-
-        print("\n===== CATEGORIAS =====")
-
-        lista = []
-
-        for i, categoria in enumerate(categorias, start=1):
-            categoria = categoria.strip()
-            lista.append(categoria)
-            print(f"{i} - {categoria}")
-
-        return lista
-
-    except FileNotFoundError:
-        print("Nenhuma categoria cadastrada.")
-        return []
-
-
-
-# entrada e saida (entrada +money, saida -money)
-
-
-
-def registrar_entrada():
-    categorias = listar_categorias()
-
-    if len(categorias) == 0:
-        print("Cadastre uma categoria primeiro.")
-        return
-
-    try:
-        opcao = int(input("Escolha a categoria da entrada: "))
-
-        if opcao < 1 or opcao > len(categorias):
-            print("Categoria inválida.")
-            return
-
-        categoria = categorias[opcao - 1]
-
-        valor = float(input("Valor da entrada: R$ "))
-
-        data = datetime.now().strftime("%d/%m/%Y")
-
-        with open("movimentacoes.txt", "a", encoding="utf-8") as arquivo:
-            arquivo.write(
-                f"entrada;{valor};{categoria};{data}\n"
-            )
-
-        print("Entrada registrada com sucesso!")
-
-    except ValueError:
-        print("Valor inválido!")
-
-
-
-def registrar_saida():
-    categorias = listar_categorias()
-
-    if len(categorias) == 0:
-        return
-
-    try:
-        opcao = int(input("Escolha o número da categoria: "))
-
-        if opcao < 1 or opcao > len(categorias):
-            print("Categoria inválida.")
-            return
-
-        categoria = categorias[opcao - 1]
-
-        valor = float(input("Valor da saída: R$ "))
-
-        data = datetime.now().strftime("%d/%m/%Y")
-
-        with open("movimentacoes.txt", "a", encoding="utf-8") as arquivo:
-            arquivo.write(f"saida;{valor};{categoria};{data}\n")
-
-        print("Saída registrada com sucesso!")
-
-    except ValueError:
-        print("Valor inválido!")
-
-
-
-# relatorio (soma das entrada, saida e o resultado do saldo)
-
-
-
 def relatorio():
-    entradas = 0
-    saidas = 0
+   try:
+      with open("transacoes.json", "r") as f:
+           transacoes = json.load(f)
+   except FileNotFoundError:
+        print("Nenhuma transação registrada.\n")
+        return
+   
+   mes_atual = datetime.now().strftime("%m/%Y")
 
-    try:
-        with open("movimentacoes.txt", "r", encoding="utf-8") as arquivo:
+   print(f"\n-- Relatório: {mes_atual} --")
 
-            for linha in arquivo:
-                dados = linha.strip().split(";")
+   total_entradas = 0
+   total_saidas = 0
 
-                if dados[0] == "entrada":
-                    entradas += float(dados[1])
+   for t in transacoes:
+       if t["data"].endswith(mes_atual):
+          if t["tipo"] == "entrada" :
+             total_entradas += t["valor"]
+          else:
+             total_saidas += t["valor"]
+             label = "Saídas"
+          print(f"{label}: {t ['data']} - {t['categoria']} - {t['descrição']} - R$ {t['valor']:.2f}")
+  
+   saldo = total_entradas - total_saidas
 
-                elif dados[0] == "saida":
-                    saidas += float(dados[1])
+   print(f"\nTotal Entradas : R$ {total_entradas:.2f}")
+   print(f"Total Saídas   : R$ {total_saidas:.2f}")
+   print(f"Saldo          : R$ {saldo:.2f}")
 
-        saldo = entradas - saidas
-
-        relatorio_texto = (
-            "===== RELATÓRIO FINANCEIRO =====\n"
-            f"Total de Entradas: R$ {entradas:.2f}\n"
-            f"Total de Saídas: R$ {saidas:.2f}\n"
-            f"Saldo Atual: R$ {saldo:.2f}\n"
-        )
-
-        print("\n" + relatorio_texto)
-
-        with open(
-            "relatorio_financeiro.txt",
-            "w",
-            encoding="utf-8"
-        ) as arquivo:
-            arquivo.write(relatorio_texto)
-
-        print("Relatório salvo em relatorio_financeiro.txt")
-
-    except FileNotFoundError:
-        print("Nenhuma movimentação encontrada.")
-
-
-
-# movimentações
-
-
-def listar_movimentacoes():
-    try:
-        with open("movimentacoes.txt", "r", encoding="utf-8") as arquivo:
-
-            print("\n===== MOVIMENTAÇÕES =====")
-
-            for linha in arquivo:
-                dados = linha.strip().split(";")
-
-                print(
-                    f"Tipo: {dados[0].upper()} | "
-                    f"Valor: R$ {float(dados[1]):.2f} | "
-                    f"Categoria: {dados[2]} | "
-                    f"Data: {dados[3]}"
-                )
-
-    except FileNotFoundError:
-        print("Nenhuma movimentação cadastrada.")
-
-
-
-# menu gigante ta doido
-
-
-
-def menu():
-    while True:
-
-        print("\n==============================")
-        print(" SISTEMA DE FINANÇAS PESSOAIS ")
-        print("==============================")
-        print("1 - Cadastrar categoria")
-        print("2 - Listar categorias")
-        print("3 - Registrar entrada")
-        print("4 - Registrar saída")
-        print("5 - Listar movimentações")
-        print("6 - Relatório financeiro")
-        print("0 - Sair")
-
-        opcao = input("Escolha uma opção: ")
-
-        if opcao == "1":
-            cadastrar_categoria()
-
-        elif opcao == "2":
-            listar_categorias()
-
-        elif opcao == "3":
-            registrar_entrada()
-
-        elif opcao == "4":
-            registrar_saida()
-
-        elif opcao == "5":
-            listar_movimentacoes()
-
-        elif opcao == "6":
-            relatorio()
-
-        elif opcao == "0":
-            print("Programa encerrado.")
-            break
-
-        else:
-            print("Opção inválida.")
-
-
+   if saldo >= 0:
+        print("Saldo Positivo ✔\n")
+   else:
+        print("Saldo Negativo ✖\n")  
 menu()
->>>>>>> 06c486f786d0f012de842159e2e0f1bc48787299
+
+ 
+ 
+
